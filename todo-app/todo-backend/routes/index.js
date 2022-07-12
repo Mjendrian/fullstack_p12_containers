f0e144eb8344406e75f94eaef39a8cb3193c8fcc
Martin Jendrian <mjendrian@gmail.com>
2022-07-12 11:32:58 +0200

    Finished Part 12-b

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const configs = require('../util/config')
+const configs = require('../util/config');
+const redis = require('../redis');
+
 
 let visits = 0
 
@@ -12,6 +14,16 @@ router.get('/', async (req, res) => {
   res.send({
     ...configs,
     visits
+  });
+});
+
+/* GET statistics data. */
+router.get('/statistics', async (req, res) => {
+  const added_todos = await redis.getAsync("added_todos") || 0;
+
+  console.log(added_todos);
+  res.send({
+    "added_todos" : Number(added_todos)
   });
 });
 
--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const { Todo } = require('../mongo')
+const { Todo } = require('../mongo');
 const router = express.Router();
+const redis = require('../redis');
 
 /* GET todos listing. */
 router.get('/', async (_, res) => {
@@ -14,6 +15,14 @@ router.post('/', async (req, res) => {
     text: req.body.text,
     done: false
   })
+  console.log(todo);
+  const added_todos = await redis.getAsync("added_todos") || 0;
+  console.log(added_todos);
+  if(!added_todos) {
+    await redis.setAsync("added_todos", 1);
+  }else{
+    await redis.setAsync("added_todos", Number(added_todos) + 1);
+  }
   res.send(todo);
 });
 
@@ -35,13 +44,28 @@ singleRouter.delete('/', async (req, res) => {
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  res.send(req.todo);
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  console.log(req.body);
+  console.log(req.todo.id);
+
+  const updateCallback = (err, todo) => {
+    if (err){
+      res.status(400);
+      res.send(err);
+    } else {
+      res.status(200);
+      res.send(todo);
+    }
+  } 
+
+  const todo = await Todo.updateOne({_id:req.todo.id}, req.body, updateCallback);
+
 });
+
 
 router.use('/:id', findByIdMiddleware, singleRouter)
 
